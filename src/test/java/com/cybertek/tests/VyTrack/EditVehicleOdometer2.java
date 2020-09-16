4a6feeaa0f75a700ad53e7322ac3f1b55ad586c7
Alex-B-I <alex.berloh@gmail.com>
2020-09-16 11:42:39 -0500

    Comment

--- src/test/java/com/cybertek/tests/VyTrack/EditVehicleOdometer2.java
+++ src/test/java/com/cybertek/tests/VyTrack/EditVehicleOdometer2.java
@@ -25,6 +25,7 @@ public class EditVehicleOdometer2 {
         driver.get("https://qa2.vytrack.com/user/login");
         driver.findElement(By.id("prependedInput")).sendKeys("user161");
         driver.findElement(By.id("prependedInput2")).sendKeys("UserUser123" + Keys.ENTER);
+
     }
 
     @Test
